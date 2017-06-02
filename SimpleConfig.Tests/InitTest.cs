2f2e52dd742b26456d1d21d2dc15723d9423aabc
Michał Pietrowicz <michalpietrowicz@outlook.com>
2017-06-02 17:12:19 +0200

    Init test project

---
+++ SimpleConfig.Tests/InitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleConfig.Tests
+{
+    /// <summary>
+    /// Test for file save config
+    /// </summary>
+    [TestClass]
+    public class InitTest
+    {
+        /// <summary>
+        /// Check if interfase is not null and are same
+        /// </summary>
+        [TestMethod]
+        public void Init()
+        {
+            Assert.IsNotNull(SimpleConfig.Init(),"SimpleConfig.Init() != null library returns interface");
+            Assert.AreEqual(SimpleConfig.Init(), SimpleConfig.Init(), "Interfaces are not the same");
+        }
+
+        public void SoureFromFile()
+        {
+            
+        }
+    }
+}
---
+++ SimpleConfig/SimpleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleConfig
+{
+    /// <summary>
+    /// Main initjalize class
+    /// </summary>
+    public class SimpleConfig
+    {
+        /// <summary>
+        /// Singelton object to return only internal acces
+        /// </summary>
+        internal static IConfig conf = new MainInit();
+        /// <summary>
+        /// Main config initjalization no settings present
+        /// </summary>
+        /// <returns>Interface for configuration</returns>
+        public static IConfig Init()
+        {
+            return conf;
+        }
+        /// <summary>
+        /// Constructor not welcome
+        /// </summary>
+        private SimpleConfig()
+        {
+            
+        }
+        
+    }
+}
